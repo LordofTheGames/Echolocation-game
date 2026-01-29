0dd4f2a824d689d6a80f3685df8a7584ee41fb1f
Asher Bloom <si23870@bristol.ac.uk>
2026-01-29 21:52:14 +0000

    rename ground to obstacle and try to get fov working

---
+++ Assets/Scripts/Behaviour/FieldOfViewAction.cs
@@ -0,0 +1,37 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "field of view", story: "Check if [Target] is in field of view of [Agent] and set [FollowTarget]", category: "Action", id: "2de421468d7cef870074735fc8379e2a")]
+public partial class FieldOfViewAction : Action
+{
+    [SerializeReference] public BlackboardVariable<GameObject> Target;
+    [SerializeReference] public BlackboardVariable<GameObject> Agent;
+    [SerializeReference] public BlackboardVariable<GameObject> FollowTarget;
+    [SerializeReference] public BlackboardVariable<FieldOfView> FovScript;
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        bool canSee = FovScript.Value.FieldOfViewCheck(Target.Value);
+        if (canSee)
+        {
+            FollowTarget.Value = Target.Value;
+            return Status.Success;
+        }
+        FollowTarget.Value = null;
+        return Status.Failure;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float Radius;
+    [Range(0,360)]
+    public float Angle;
+    public float DetectionHeight;
+    public LayerMask ObstructionMask;
+    public bool ShowDebugVisuals;
+    private void Start()
+    {
+    }
+
+    // Returns true if Target can be seen, null otherwise
+    public bool FieldOfViewCheck(GameObject target)
+    {
+        int targetMask = 1 << target.layer;
+        Collider[] collidersInRange = Physics.OverlapSphere(transform.position, Radius, targetMask);
+        for (int i = 0; i < collidersInRange.Length; i++)
+        {
+            if (collidersInRange[i].gameObject == target)
+            {
+                return true;
+                Vector3 directionToTarget = (target.transform.position - transform.position).normalized;
+                if (Vector3.Angle(transform.forward, directionToTarget) < Angle / 2)
+                {
+                    float distanceToTarget = Vector3.Distance(transform.position, target.transform.position);
+                    bool viewObstructed = Physics.Raycast(transform.position + Vector3.up * DetectionHeight, directionToTarget, distanceToTarget, ObstructionMask);
+                    if (!viewObstructed)
+                        return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (ShowDebugVisuals)
+        {
+            Gizmos.color = Color.red;
+            Vector3 viewAngle01 = DirectionFromAngle(transform.eulerAngles.y, -Angle / 2);
+            Vector3 viewAngle02 = DirectionFromAngle(transform.eulerAngles.y, Angle / 2);
+
+            Gizmos.DrawLine(transform.position + Vector3.up * DetectionHeight, transform.position + viewAngle01 * Radius + Vector3.up * DetectionHeight);
+            Gizmos.DrawLine(transform.position + Vector3.up * DetectionHeight, transform.position + viewAngle02 * Radius + Vector3.up * DetectionHeight);
+            Gizmos.DrawWireSphere(transform.position + Vector3.up * DetectionHeight, Radius);
+        }
+    }
+    private Vector3 DirectionFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
