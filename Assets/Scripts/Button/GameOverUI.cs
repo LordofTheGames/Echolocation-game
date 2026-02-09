23a77138845bb3faab0b4f3c1fb38c8df59bf8af
Yuzheng Liu <tv23570@bristol.ac.uk>
2026-02-09 14:49:23 +0000

    Add an exit button and cursor

--- Assets/Scripts/Button/GameOverUI.cs
+++ Assets/Scripts/Button/GameOverUI.cs
@@ -3,8 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class GameOverUI : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void OnRestartClicked()
     {
         SceneManager.LoadScene("MVP");
+    }
+
+    public void OnExitClicked()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
