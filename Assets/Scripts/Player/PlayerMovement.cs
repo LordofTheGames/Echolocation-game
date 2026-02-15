c2a6812be08bc6b9e6352ac06c9f954512ee8ea7
tommyd1123 <of23238@bristol.ac.uk>
2026-02-15 21:56:16 +0000

    changing speed feature

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,9 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
-    public float speed = 12f;
+    public float crouchSpeed = 3f;
+    public float walkSpeed = 6f;
+    public float sprintSpeed = 12f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
     public bool jumpEnabled = false;
@@ -16,7 +18,33 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
     Vector2 moveInput;  // Stores WASD input
+
+    private bool isCrouching = false;
+    private bool isSprinting = false;
+
+    public Transform cameraTransform;
+    private float defaultCamY;
+
     
+
+    // hold shift
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            isSprinting = true;
+
+        if (context.canceled)
+            isSprinting = false;
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+            isCrouching = true;
+
+        if (context.canceled)
+            isCrouching = false;
+    }
 
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -39,9 +67,21 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        defaultCamY = cameraTransform.localPosition.y;
+    }
     // Update is called once per frame
     void Update()
     {
+
+        // change height when crouching
+        float targetY = isCrouching ? defaultCamY - 0.3f : defaultCamY;
+
+        Vector3 camPos = cameraTransform.localPosition;
+        camPos.y = Mathf.Lerp(camPos.y, targetY, Time.deltaTime * 10);
+        cameraTransform.localPosition = camPos;
+
         //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         if (isGrounded && velocity.y < 0)
@@ -49,8 +89,19 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = 0f;
         }
 
+        // if crouching, sprinting is not allowed
+        bool sprintAllowed = !isCrouching;
+
+        float currentSpeed = walkSpeed;
+
+        if (isCrouching)
+            currentSpeed = crouchSpeed;
+        else if (sprintAllowed && isSprinting)
+            currentSpeed = sprintSpeed;
+
         Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
-        controller.Move(speed * Time.deltaTime * move);
+        controller.Move(currentSpeed * Time.deltaTime * move);
+
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
