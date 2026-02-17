cd5156d1d756e1c263192723a93440c58127ce90
Asher Bloom <si23870@bristol.ac.uk>
2026-02-17 23:22:37 +0000

    merge everything

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -40,7 +40,11 @@ public class PlayerMovement : MonoBehaviour
     public void OnCrouch(InputAction.CallbackContext context)
     {
         if (context.performed)
+        {
+            Debug.Log("AHAHA");
             isCrouching = !isCrouching; 
+        }
+             
     }
 
     public void OnMove(InputAction.CallbackContext context)
