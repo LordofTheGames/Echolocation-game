b271dee005e04d794c06c447efd5482d7873dc93
Asher Bloom <si23870@bristol.ac.uk>
2026-02-17 21:18:23 +0000

    fix throwitem and make sounds louder

--- Assets/Scripts/Player/ThrowItem.cs
+++ Assets/Scripts/Player/ThrowItem.cs
@@ -204,21 +204,6 @@ public class ThrowItem : MonoBehaviour
         {
             currentObj = Instantiate(prefab, cubeSpawnPoint.position, Quaternion.identity);
             currentObj.transform.SetParent(cubeSpawnPoint);
-
-            
-
-            Rigidbody rb = currentObj.GetComponent<Rigidbody>();
-            if (rb != null)
-                rb.isKinematic = true;
-
-            MeshRenderer cubeRenderer = currentObj.GetComponent<MeshRenderer>();
-            if (cubeRenderer != null)
-            {
-                Material cubeMat = cubeRenderer.material;
-                Color cubeColor = cubeMat.color;
-                cubeColor.a = 0.8f;
-                cubeMat.color = cubeColor;
-            }
         }
 
         trajectoryLine.enabled = true;
