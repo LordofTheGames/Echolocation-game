edb43793334483e79e84135aac0690f401ef8024
Asher Bloom <si23870@bristol.ac.uk>
2026-02-17 18:10:06 +0000

    Change keybindings and make crouch toggle

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -40,10 +40,7 @@ public class PlayerMovement : MonoBehaviour
     public void OnCrouch(InputAction.CallbackContext context)
     {
         if (context.performed)
-            isCrouching = true;
-
-        if (context.canceled)
-            isCrouching = false;
+            isCrouching = !isCrouching; 
     }
 
     public void OnMove(InputAction.CallbackContext context)
