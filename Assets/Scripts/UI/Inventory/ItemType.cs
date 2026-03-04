ad214e8f086356681a2de69fdb1d7d2190c26e0d
Asher Bloom <si23870@bristol.ac.uk>
2026-03-04 04:09:30 +0000

    fix dodgyness

--- Assets/Scripts/UI/Inventory/ItemType.cs
+++ Assets/Scripts/UI/Inventory/ItemType.cs
@@ -3,13 +3,8 @@ public enum ItemType
     Rock,
     Clicker,
     Plank,
-<<<<<<< controller-mapping
-    Flashbang, 
-    Key,
-    None
-=======
     SonicGrenade,
     Key,
-    EchoBeacon
->>>>>>> dev
+    EchoBeacon,
+    None
 }
