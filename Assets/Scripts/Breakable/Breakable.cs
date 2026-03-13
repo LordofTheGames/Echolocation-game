ac28fe506fb7061a79942dc86bbdabd74e0db2ed
Yuzheng Liu <tv23570@bristol.ac.uk>
2026-03-13 02:17:46 +0000

    Modify the crushing method

--- Assets/Scripts/Breakable/Breakable.cs
+++ Assets/Scripts/Breakable/Breakable.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 [SelectionBase]
 public class Breakable : MonoBehaviour
 {
+    public enum BreakableType { Rock, Glass }
+
     [SerializeField] GameObject box;
     [SerializeField] GameObject brokenBox;
     [SerializeField] AudioClip breakSound;
+    [SerializeField] BreakableType breakableType = BreakableType.Rock;
+    [SerializeField] MicInput micInput;
+    [SerializeField] float minVolumeToBreak = 0.4f;
+    [SerializeField] float minPitchToBreak = 100f;
+    [SerializeField] float holdTimeToBreak = 1.2f;
+
     BoxCollider bc;
     AudioSource _audioSource;
+    bool _hasBroken;
+    float _holdTimer;
 
     private void Awake()
     {
@@ -21,13 +31,32 @@ public class Breakable : MonoBehaviour
         }
     }
 
-    private void OnMouseDown()
+    private void Update()
     {
-        Break();
+        if (_hasBroken || micInput == null) return;
+
+        bool conditionMet = false;
+
+        if (breakableType == BreakableType.Rock && micInput.volume >= minVolumeToBreak)
+            conditionMet = true;
+        else if (breakableType == BreakableType.Glass && micInput.pitchHz >= minPitchToBreak)
+            conditionMet = true;
+
+        if (conditionMet)
+        {
+            _holdTimer += Time.deltaTime;
+            if (_holdTimer >= holdTimeToBreak)
+                Break();
+        }
+        else
+        {
+            _holdTimer = 0f;
+        }
     }
 
     private void Break()
     {
+        _hasBroken = true;
         box.SetActive(false);
         brokenBox.SetActive(true);
         bc.enabled = false;
