8b3f3cca3607d1cf5cd49aa7cc24dfe64d154583
Asher Bloom <si23870@bristol.ac.uk>
2026-03-18 13:26:42 +0000

    final changes

--- Assets/Scripts/Behaviour/FindNavigablePositionAction.cs
+++ Assets/Scripts/Behaviour/FindNavigablePositionAction.cs
@@ -14,9 +14,11 @@ public partial class FindNavigablePositionAction : Action
     [SerializeReference] public BlackboardVariable<float> Radius = new BlackboardVariable<float>(10.0f);
 
     private float checkCount;
+    private NavMeshAgent agent;
 
     protected override Status OnStart()
     {
+        agent = Agent.Value.GetComponent<NavMeshAgent>();
         checkCount = 0;
         return Status.Running;
     }
@@ -33,10 +35,17 @@ public partial class FindNavigablePositionAction : Action
         checkCount++;
         if(NavMesh.SamplePosition(randomPosition, out hit, maxSearchDist, NavMesh.AllAreas))
         {
-            FinalPoint.Value = hit.position;
-            return Status.Success;
+            NavMeshPath path = new NavMeshPath();
+            if (agent.CalculatePath(hit.position, path))
+            {
+                if (path.status == NavMeshPathStatus.PathComplete)
+                {
+                    FinalPoint.Value = hit.position;
+                    return Status.Success;
+                }
+            }
         }
-        else if (checkCount == 5)
+        if (checkCount == 5)
         {
             FinalPoint.Value = Agent.Value.transform.position;
             return Status.Failure;
