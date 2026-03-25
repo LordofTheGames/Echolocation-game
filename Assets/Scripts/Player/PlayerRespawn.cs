b70dfc1539a676247cc676f5b343343791860b2a
Emmanuel Destin <emmanuel.destin21@gmail.com>
2026-03-25 00:36:50 +0000

    made a death animation

--- Assets/Scripts/Player/PlayerRespawn.cs
+++ Assets/Scripts/Player/PlayerRespawn.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using Unity.Behavior;
 
 public class PlayerRespawn : MonoBehaviour
@@ -8,38 +9,82 @@ public class PlayerRespawn : MonoBehaviour
     public HideInBox CurrentBox;
     public static event System.Action OnPlayerExit;
     private GameObject navmeshEdges;
+    public Image blackScreenImage; 
+    public float fadeWaitTime = 1.0f; 
+
+    private bool isRespawning = false;
 
     void Start()
     {
         navmeshEdges = GameObject.Find("NavMesh Edges");
+        
+        if (blackScreenImage != null)
+        {
+            Color c = blackScreenImage.color;
+            c.a = 0f;
+            blackScreenImage.color = c;
+        }
     }
 
     public void Respawn()
     {
-        // reset all hiding variables etc. if hiding
+        if (!isRespawning) StartCoroutine(RespawnSequence());
+    }
+
+    private IEnumerator RespawnSequence()
+    {
+        isRespawning = true;
+
+        PlayerMovement pm = gameObject.GetComponent<PlayerMovement>();
+        if (pm != null) pm.enabled = false;
+
+        //fade to black
+        if (blackScreenImage != null)
+        {
+            float timer = 0f;
+            Color c = blackScreenImage.color;
+            while (timer < fadeWaitTime)
+            {
+                timer += Time.deltaTime;
+                c.a = timer / fadeWaitTime;
+                blackScreenImage.color = c;
+                yield return null; 
+            }
+        }
+
         if (CurrentBox != null && CurrentBox.isHiding && !CurrentBox.isTransitioning)
         {
             CurrentBox.isHiding = false; 
             OnPlayerExit?.Invoke();
 
-            CharacterController cc = gameObject.GetComponent<CharacterController>();
-            cc.enabled = false;
-            transform.position = RespawnPosition;
-            cc.enabled = true;
-
-            // Re-enable player movements
-            gameObject.GetComponent<PlayerMovement>().enabled = true;
             MouseLook ml = gameObject.GetComponentInChildren<MouseLook>();
-            ml.isHiding = false; 
-            ml.hidingTransition = false; 
-            navmeshEdges.SetActive(true);
+            if (ml != null) { ml.isHiding = false; ml.hidingTransition = false; }
+            if (navmeshEdges != null) navmeshEdges.SetActive(true);
         }
-        else
+
+        CharacterController cc = gameObject.GetComponent<CharacterController>();
+        cc.enabled = false;
+        transform.position = RespawnPosition;
+        cc.enabled = true;
+
+
+        //fade back in
+        if (blackScreenImage != null)
         {
-            CharacterController cc = gameObject.GetComponent<CharacterController>();
-            cc.enabled = false;
-            transform.position = RespawnPosition;
-            cc.enabled = true;
+            float timer = 0f;
+            Color c = blackScreenImage.color;
+            while (timer < fadeWaitTime)
+            {
+                timer += Time.deltaTime;
+                c.a = 1f - (timer / fadeWaitTime); 
+                blackScreenImage.color = c;
+                yield return null;
+            }
         }
+
+        if (pm != null) pm.enabled = true;
+        isRespawning = false; 
     }
 }
+
+
