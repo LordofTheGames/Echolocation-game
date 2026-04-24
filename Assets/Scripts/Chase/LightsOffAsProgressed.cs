b4bffd19e12c90ccaa0da8a2af1881403970ae50
KieranDunning <pl23235@bristol.ac.uk>
2026-04-25 00:09:25 +0100

    Hopefully fixed final chase lights not turning off issue

--- Assets/Scripts/Chase/LightsOffAsProgressed.cs
+++ Assets/Scripts/Chase/LightsOffAsProgressed.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 [RequireComponent(typeof(Light))]
 public class LightTrigger : MonoBehaviour
 {
-    private Light spotLight;
+    public Light spotLight;
 
     private float delayTime = 0.5f;
-
-    void Start()
-    {
-        // Get the Light component attached to this GameObject
-        spotLight = GetComponentInParent<Light>();
-    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -34,6 +28,10 @@ public class LightTrigger : MonoBehaviour
             // This turns off the entire GameObject holding the light,
             // so the FlashLights script cannot force it to turn on again
             spotLight.gameObject.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("No light assigned! Make sure to drag 'Spot Light' into the slot in Inspector.");
         }
     }
 }
